3fcc49b561ec47422fd295cb0425e8e66224e82c
jtauchus <58740641+jtauchus@users.noreply.github.com>
2024-10-07 19:12:20 -0400

    h

--- skin.js
+++ skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 7.1.4/20938
 // Filename: PMC.ggsk
-// Generated 2024-10-07T18:14:54
+// Generated 2024-10-07T19:11:57
 
 function pano2vrSkin(player,base) {
 	var me=this;
